1e18cf58d95bc75dd73ffebcf306d22e2a44ea63
Srikanth Chakravarthy <srikanth.chakravarthy@ibm.com>
2025-07-07 17:27:08 +0530

    Updated with locking of files and archive

--- bin/run-sevone-ingest-data.sh
+++ bin/run-sevone-ingest-data.sh
@@ -1,3 +1,5 @@
 podman run --rm \
   -v /var/custom/ps-addons/APPNAMEREPLACE/etc:/app/etc:ro \
+  -v /var/custom/ps-addons/APPNAMEREPLACE/input:/app/input:ro \
+  -v /var/custom/ps-addons/APPNAMEREPLACE/archive:/app/archive:rw \
   APPNAMEREPLACE
--- src/logger_config.py
+++ src/logger_config.py
@@ -6,8 +6,8 @@ from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 import json
 
 # Create a logger
-#file_prefix = "/app/"
-file_prefix = ""
+file_prefix = "/app/"
+#file_prefix = ""
 configurationFile = file_prefix + "etc/config.json"
 with open(configurationFile, "r") as f:
     try:
