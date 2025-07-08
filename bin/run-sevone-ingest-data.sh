42698e1895d1bbd97ba3bba3ed14de726bc136e4
Srikanth Chakravarthy <srikanth.chakravarthy@ibm.com>
2025-07-08 11:15:38 +0530

    Added requirements txt

--- bin/run-sevone-ingest-data.sh
+++ bin/run-sevone-ingest-data.sh
@@ -1,5 +1,22 @@
-podman run --rm \
-  -v /var/custom/ps-addons/APPNAMEREPLACE/etc:/app/etc:ro \
-  -v /var/custom/ps-addons/APPNAMEREPLACE/input:/app/input:ro \
-  -v /var/custom/ps-addons/APPNAMEREPLACE/archive:/app/archive:rw \
-  APPNAMEREPLACE
+  #!/bin/bash
+APP_NAME="APPNAMEREPLACE"
+IMAGE_NAME="CONTAINERIMAGEREPLACE:CONTAINERIMAGEVERSIONREPLACE"
+HOST_IP=${HOST_IP}
+
+
+
+/usr/bin/podman run --rm \
+  --name "$APP_NAME" \
+  --user 0:0 \
+  --memory 801337k \
+  --log-driver=json-file \
+  --log-opt max-size=20m \
+  --log-opt max-file=10 \
+  -v /var/custom/ps-addon/$APP_NAME/etc:/app/etc \
+  -v /var/custom/ps-addon/$APP_NAME/env:/app/env \
+  -v /var/custom/ps-addon/$APP_NAME/bin:/app/bin \
+  -v /var/custom/ps-addon/$APP_NAME/log:/app/log \
+  -v /var/custom/ps-addon/$APP_NAME/input:/app/input:ro \
+  -v /var/custom/ps-addon/$APP_NAME/archive:/app/archive:rw \
+  -e HOST_IP="$HOST_IP" \
+  "$IMAGE_NAME"
